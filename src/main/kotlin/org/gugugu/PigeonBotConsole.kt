7a2b4d95160c1c80bd50302307d40b37cda08413
willbe <aotuman233@gmail.com>
2021-05-07 01:50:51 +0800

    files & folder initialize

--- src/main/kotlin/org/gugugu/PigeonBotConsole.kt
+++ src/main/kotlin/org/gugugu/PigeonBotConsole.kt
@@ -8,6 +8,7 @@ import net.mamoe.mirai.utils.info
 import org.gugugu.KeywordAutoReply.*
 import org.gugugu.config.Config
 import org.gugugu.config.ConfigCommand
+import java.io.File
 
 object PigeonBotConsole : KotlinPlugin(
     JvmPluginDescription(
@@ -18,6 +19,7 @@ object PigeonBotConsole : KotlinPlugin(
 ) {
     override fun onEnable() {
         logger.info { "Plugin loaded" }
+        initFolders()
         // load configs
         KeywordData.reload()
         Config.reload()
@@ -32,5 +34,20 @@ object PigeonBotConsole : KotlinPlugin(
 
     override fun onDisable() {
         CommandManager.INSTANCE.unregisterAllCommands(PigeonBotConsole)
+    }
+
+    fun initFolders(){
+        fun createFolder (vararg imageFolder:File){
+            for (folder in imageFolder) {
+                if (!folder.exists())
+                    if (!folder.mkdirs())
+                        logger.error("创建图片文件夹失败。")
+            }
+        }
+        val imageFolder = this.resolveDataFile("images/")
+        val catFolder = this.resolveDataFile("images/cats")
+        val quoteFolder = this.resolveDataFile("images/quotes")
+        val replyFolder = this.resolveDataFile("images/replies")
+        createFolder(imageFolder,catFolder,quoteFolder,replyFolder)
     }
 }
